fe6f1a9a42944af70301ec54eaf8517f35cd91da
etienne <laurentlionel20@hotmail.com>
2024-11-05 14:47:03 +0100

    [Add] exo condition

--- day2/exo1/Program.cs
+++ day2/exo1/Program.cs
@@ -26,56 +26,65 @@ static int getUserInput(string message){
     return val;
 }
 
+static string evenOrOdd(int number){
+    if(number %2 == 0){
+        return "Pair";
+    }
+    return "Impaire";
+}
+
 #region exo1 (tryparse)
 int first_numer = getUserInput("Veulliez introduire un premier nombre entier");
 int second_number = getUserInput("Veulliez introduire un second nombre entier");
-
-Console.WriteLine($"Result = {first_numer + second_number}");
+int res = first_numer + second_number;
+Console.WriteLine($"Result = {res}\nLe resultat est {evenOrOdd(res)}");
 
 #endregion
 
 
-static int getUserInputParse(string message){
-    string? user_input; 
-    int val = 0;
+// static int getUserInputParse(string message){
+//     string? user_input; 
+//     int val = 0;
 
-    do
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(message);
-        Console.ResetColor();
+//     do
+//     {
+//         Console.ForegroundColor = ConsoleColor.Red;
+//         Console.WriteLine(message);
+//         Console.ResetColor();
 
-        user_input = Console.ReadLine();
+//         user_input = Console.ReadLine();
         
-        try
-        {
-            if (user_input is null){
-                Console.WriteLine("Invalid input. Please enter a valid integer.");
-                continue;
-            }
-            val = int.Parse(user_input );
-            break;
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Invalid input. Please enter a valid integer.");
-            continue;
-        }
-    } while (true);
-    return val;
+//         try
+//         {
+//             if (user_input is null){
+//                 Console.WriteLine("Invalid input. Please enter a valid integer.");
+//                 continue;
+//             }
+//             val = int.Parse(user_input );
+//             break;
+//         }
+//         catch (FormatException)
+//         {
+//             Console.WriteLine("Invalid input. Please enter a valid integer.");
+//             continue;
+//         }
+//     } while (true);
+//     return val;
 
-}
+// }
 
-Thread.Sleep(200);
-Console.Clear();
+// Thread.Sleep(200);
+// Console.Clear();
 
 
 
-#region exo1 (parse)
-int first_val = getUserInputParse("Veulliez introduire un premier nombre entier");
-int second_val= getUserInputParse("Veulliez introduire un second nombre entier");
+// #region exo1 (parse)
+// int first_val = getUserInputParse("Veulliez introduire un premier nombre entier");
+// int second_val= getUserInputParse("Veulliez introduire un second nombre entier");
 
-Console.WriteLine($"Result = {first_val + second_val}");
+// Console.WriteLine($"Result = {first_val + second_val}\n Le resultat est {}");
 
-#endregion
+// #endregion
+
+
 
--- day2/exo1/obj/Debug/net8.0/exo1.AssemblyInfo.cs
+++ day2/exo1/obj/Debug/net8.0/exo1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ede1c79523e3fbe55d290624e484db521646fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b186f1d8eebf01f0aafe450b7ae7d2f2468824e")]
 [assembly: System.Reflection.AssemblyProductAttribute("exo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
