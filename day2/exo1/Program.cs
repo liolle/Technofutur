3b186f1d8eebf01f0aafe450b7ae7d2f2468824e
etienne <laurentlionel20@hotmail.com>
2024-11-05 14:04:50 +0100

    [Update] exo1

--- day2/exo1/Program.cs
+++ day2/exo1/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static int getUserInput(string message){
+
+    string? user_input;
+    int val = 0;
+
+    do
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+
+        user_input = Console.ReadLine();
+        
+
+        if (int.TryParse(user_input,out val))
+        {
+            break;
+            
+        }else {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+
+     
+    } while (true);
+    return val;
+}
+
+#region exo1 (tryparse)
+int first_numer = getUserInput("Veulliez introduire un premier nombre entier");
+int second_number = getUserInput("Veulliez introduire un second nombre entier");
+
+Console.WriteLine($"Result = {first_numer + second_number}");
+
+#endregion
+
+
+static int getUserInputParse(string message){
+    string? user_input; 
+    int val = 0;
+
+    do
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+
+        user_input = Console.ReadLine();
+        
+        try
+        {
+            if (user_input is null){
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                continue;
+            }
+            val = int.Parse(user_input );
+            break;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+            continue;
+        }
+    } while (true);
+    return val;
+
+}
+
+Thread.Sleep(200);
+Console.Clear();
+
+
+
+#region exo1 (parse)
+int first_val = getUserInputParse("Veulliez introduire un premier nombre entier");
+int second_val= getUserInputParse("Veulliez introduire un second nombre entier");
+
+Console.WriteLine($"Result = {first_val + second_val}");
+
+#endregion
+
--- day2/exo1/obj/Debug/net8.0/exo1.AssemblyInfo.cs
+++ day2/exo1/obj/Debug/net8.0/exo1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4a7b5b083620b5587f530c46e4da5e9841dadb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ede1c79523e3fbe55d290624e484db521646fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("exo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
