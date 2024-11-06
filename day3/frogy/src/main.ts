6be0db71600397ff50130b1544e1a37b1b5c11ca
etienne <laurentlionel20@hotmail.com>
2024-11-06 17:08:43 +0100

    [Add] frogy scafolding

---
+++ day3/frogy/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div>
+	Hello world	
+</div>
+`
+
