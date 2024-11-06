84fd328264d9ed79a39f644d08858b476564eacd
etienne <laurentlionel20@hotmail.com>
2024-11-06 17:58:37 +0100

    [Add] canvas

--- day3/frogy/src/main.ts
+++ day3/frogy/src/main.ts
@@ -2,9 +2,3 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>
-	Hello world	
-</div>
-`
-
