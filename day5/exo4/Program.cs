1d9d65fe8e3d0045ad58ede098b6059ba477a8e9
etienne <laurentlionel20@hotmail.com>
2024-11-08 12:33:19 +0100

    [Add] day5 exo4

---
+++ day5/exo4/Program.cs
@@ -0,0 +1,39 @@
+﻿long fibonacci(int val){
+    return fibonacciR(val,1,1);
+}
+
+long fibonacciR(int val,long n_1, long n_2){
+    if (val<=0){
+        return n_1;
+    }
+
+    return fibonacciR(val-1,n_2,n_1+n_2);
+}
+
+
+void getUserInput(){
+    string? user_input;
+    int val;
+
+    do
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Pick a number ?");
+        Console.ResetColor();
+        user_input = Console.ReadLine();
+
+        if (int.TryParse(user_input, out val))
+        {
+            break;
+        }else {
+            Console.WriteLine("Invalid input. Pick a valid interger");
+        }
+
+     
+    } while (true);
+
+    Console.WriteLine($"Answer: {fibonacci(val)}");
+
+}
+
+getUserInput();
